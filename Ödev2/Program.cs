c82e16e99689141b54ebf90c10c0b80633ef13ce
Melik Ali Turan <melikalituran26@gmail.com>
2024-10-12 10:32:03 +0300

    Proje dosyası ekle.

---
+++ Ödev2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+
+    static void Main()
+    {
+        // Kullanıcıdan kenar uzunluğu al
+        Console.Write("Karenin bir kenar uzunluğunu girin: ");
+        int kenar = Convert.ToInt32(Console.ReadLine());
+
+        // Alan hesapla 
+        int alan = kenar * kenar;
+
+        // Sonucu ekrana yaz
+        Console.WriteLine($"Karenin alanı: {alan}");
+
+    }
+}
+
